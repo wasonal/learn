5c6dd6ed98c5e11991a23701ff978d84cebce9f4
denghuawen3 <denghuawen3@jd.com>
2022-04-20 10:47:27 +0800

    更新反转链表

--- 算法/practice/ListNode.ts
+++ 算法/practice/ListNode.ts
@@ -1,14 +1,14 @@
 export class ListNode {
     val: number;
-    next?: ListNode;
-    constructor(val: number, node?: ListNode) {
+    next: ListNode | null;
+    constructor(val: number, node: ListNode | null) {
         this.val = val;
         this.next = node;
     }
 }
 
 export const initListNode = (arr: number[]) => {
-    const nodeArr = arr.map((v) => new ListNode(v));
+    const nodeArr = arr.map((v) => new ListNode(v, null));
     nodeArr.reduce((pre, cur) => {
         pre.next = cur;
         return cur;
--- 算法/practice/reverseList.ts
+++ 算法/practice/reverseList.ts
@@ -1,21 +1,37 @@
+import { initListNode } from './ListNode';
 class ListNode {
     val: number;
-    next?: ListNode;
-    constructor(val: number, node?: ListNode) {
+    next: ListNode | null;
+    constructor(val: number, node: ListNode | null) {
         this.val = val;
         this.next = node;
     }
 }
 
- function reverseList(head: ListNode | null): ListNode | null {
-    let cur = head;
-    let pre: ListNode | null = null;
-    while (cur) {
-        const next = cur.next;
-        cur.next = pre;
-        pre = cur;
-        cur = next;
-    }
+// function reverseList(head: ListNode | null): ListNode | null {
+//     let cur = head;
+//     let pre: ListNode | null = null;
+//     while (cur) {
+//         const next = cur.next;
+//         cur.next = pre;
+//         pre = cur;
+//         cur = next;
+//     }
 
-    return pre;
+//     return pre;
+// };
+
+function reverseList(head: ListNode | null): ListNode | null {
+    debugger;
+    if(head === null || head.next === null) {
+        return head;
+    }
+    const next = reverseList(head.next);
+    head.next = next && next.next;
+    next && (next.next = head);
+
+    return head;
 };
+
+const lists = initListNode([1,2,3,4,5]);
+console.log(reverseList(lists));
